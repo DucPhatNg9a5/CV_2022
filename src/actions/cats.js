1a28f2528caaa1a2f4258bd6212a681fef19564e
phat nguyen <you@example.com>
2020-06-02 23:02:15 -0600

    use axios instead of fetch

--- src/actions/cats.js
+++ src/actions/cats.js
@@ -5,15 +5,26 @@ import {
   CHANGE_SEARCH_FIELD,
 } from "../actions/types";
 
+import axios from "axios";
+
 export const requestCats = () => async (dispatch) => {
   try {
     dispatch({ type: REQUEST_CATS_PENDING });
-    await fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: REQUEST_CATS_SUCCESS, payload: data }))
-      .catch((err) => dispatch({ type: REQUEST_CATS_FAILED, payload: err }));
+    // await fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((res) => res.json())
+    //   .then((data) => dispatch({ type: REQUEST_CATS_SUCCESS, payload: data }))
+    //   .catch((err) => dispatch({ type: REQUEST_CATS_FAILED, payload: err }));
+
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    dispatch({
+      type: REQUEST_CATS_SUCCESS,
+      payload: res.data,
+    });
   } catch (err) {
-    console.log(err);
+    dispatch({
+      type: REQUEST_CATS_FAILED,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
   }
 };
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,13 @@
 export const projects = [
   {
     id: 1,
-    name: "Cat Searching",
-    resource: "React-Redux",
-    description: "Using React-Redux to manage the changing stage of search bar",
-    img: "https://image.flaticon.com/icons/svg/882/882988.svg",
+    name: "Live Chat Rooms",
+    resource: "Firebase",
+    description:
+      "Multiple interactive chat rooms with real-time database (Firebase/FireStore)",
+    img: "https://image.flaticon.com/icons/svg/2937/2937072.svg",
     language: "Javascript",
-    link: "/search",
+    link: "/chat",
   },
   {
     id: 2,
@@ -20,16 +21,6 @@ export const projects = [
   },
   {
     id: 3,
-    name: "Live Chat Rooms",
-    resource: "Firebase",
-    description:
-      "Multiple interactive chat rooms with real-time database (Firebase/FireStore)",
-    img: "https://image.flaticon.com/icons/svg/2937/2937072.svg",
-    language: "Javascript",
-    link: "/chat",
-  },
-  {
-    id: 4,
     name: "Devconnector",
     resource: "MongoDB, Express, React, Node.js",
     description:
@@ -37,6 +28,15 @@ export const projects = [
     img: "https://image.flaticon.com/icons/svg/1055/1055687.svg",
     language: "Javascript",
     url: "https://devconnector-mern-stack-app.herokuapp.com/",
+  },
+  {
+    id: 4,
+    name: "Cat Searching",
+    resource: "React-Redux",
+    description: "Using React-Redux to manage the changing stage of search bar",
+    img: "https://image.flaticon.com/icons/svg/882/882988.svg",
+    language: "Javascript",
+    link: "/search",
   },
 ];
 
